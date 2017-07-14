dd806dcfef010391885cd9cb3567a34ba432a538
Tien Viet Nguyen <tien.viet.nguyen@sap.com>
2017-07-14 16:54:18 +0700

    printing...

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # learn-python
+
+learn Python from "Learn Python The Hard Way" ebook 3rd Edition.
---
+++ ex05.py
@@ -0,0 +1,10 @@
+my_name = 'Zed A Shaw'
+my_age = 35
+my_height = 74
+my_weight = 100
+my_eyes = 'blue'
+my_teeth = 'white'
+my_hair = 'brown'
+
+print ("Let's talk about %s." % my_name)
+print ("He's got %s eyes and %s hair." % (my_eyes, my_hair))
