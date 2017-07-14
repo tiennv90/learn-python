5c59c27a5fb71d0fadf149e9bf27eab27a8ac429
Tien Viet Nguyen <tien.viet.nguyen@sap.com>
2017-07-14 15:02:48 +0700

    my very first code lines of python

---
+++ ex02.py
@@ -0,0 +1,8 @@
+# a comment, this i ss so you can read your program later.
+# Anything after the # is ignored by Python
+
+print ("I could have code like this.") # and the comment after is ignored
+
+# print "This won't run"
+
+print ("This will run")
---
+++ ex03.py
@@ -0,0 +1,13 @@
+print ("I will now count my chicken")
+
+print ("Hens", 25 + 30 / 6)
+
+print ("Roosters", 100 - 25 * 3 % 4)
+
+print ("Now i will count the eggs: ")
+
+print (3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4 + 6)
+
+print ("Is it true that 3 + 2 < 5 - 7 ?")
+
+print (3 + 2 < 5 - 7)
